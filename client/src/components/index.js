716fa73c8d272af7bd7defde24140f9acf8e1054
mohanad hassan <mohanadhassan969696@gmail.com>
2023-03-21 22:37:13 +0300

    1

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,7 @@
 import Logo from "./Logo"
 import FormRow from "./FormRow"
 import Alert from'./Alert'
-export {Logo,FormRow,Alert}
+import Navbar from './Navbar'
+import BigSidebar from './BigSidebar'
+import SmallSidebar from './SmallSidebar'
+export {Logo,FormRow,Alert,Navbar,SmallSidebar,BigSidebar}
