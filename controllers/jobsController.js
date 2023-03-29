336f93f41d8182ce05a5f6d92e9dd63150950ef2
mohanad hassan <mohanadhassan969696@gmail.com>
2023-03-29 21:03:42 +0300

    stats server

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -21,43 +21,64 @@ const createJob = async (req, res) => {
 };
 const updateJob = async (req, res) => {
 
-const {id:jobID} = req.params ;
-const {company,position} = req.body ;
+    const { id: jobID } = req.params;
+    const { company, position } = req.body;
 
-if(!company || !position) {
-    throw new BadRequestError("please provide all values") ;
-}
-const job  = await Job.findOne({_id:jobID})
-if(!job) {
-    throw new NotFoundError(` no job with id${jobID} was found `)
-}
+    if (!company || !position) {
+        throw new BadRequestError("please provide all values");
+    }
+    const job = await Job.findOne({ _id: jobID })
+    if (!job) {
+        throw new NotFoundError(` no job with id${jobID} was found `)
+    }
 
-checkPermissions(req.user,job.createdBy)
-const updateJob  =await Job.findOneAndUpdate({_id:jobID},req.body,{new:true,runValidators:true})
-res.status(StatusCodes.OK).json(updateJob)
+    checkPermissions(req.user, job.createdBy)
+    const updateJob = await Job.findOneAndUpdate({ _id: jobID }, req.body, { new: true, runValidators: true })
+    res.status(StatusCodes.OK).json(updateJob)
 };
 const deleteJob = async (req, res) => {
     const { id: jobId } = req.params;
 
     const job = await Job.findOne({ _id: jobId });
-  
+
     if (!job) {
-      throw new NotFoundError(`No job with id : ${jobId}`);
+        throw new NotFoundError(`No job with id : ${jobId}`);
     }
-  
+
     checkPermissions(req.user, job.createdBy);
-  console.log(job)
-    await Job.deleteOne({_id:job._id});
-    res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' });};
+    console.log(job)
+    await Job.deleteOne({ _id: job._id });
+    res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' });
+};
 
 const getAllJobs = async (req, res) => {
 
-const jobs  = await Job.find({createdBy:req.user.userId})
-res.status(StatusCodes.OK).json({jobs,totalJobs:jobs.length , numOfPages:1})
+    const jobs = await Job.find({ createdBy: req.user.userId })
+    res.status(StatusCodes.OK).json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
 
 };
+
+
 const showStats = async (req, res) => {
-    res.send("showStats");
+    //aggregate with mongodb
+    let stats = await Job.aggregate([
+        { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+        { $group: { _id: '$status', count: { $sum: 1 } } },
+    ]);
+    //return one object contains all the values 
+    stats = stats.reduce((acc, curr) => {
+        const { _id: title, count } = curr;
+        acc[title] = count;
+        return acc;
+    }, {});
+
+    const defaultStats = {
+        pending: stats.pending || 0,
+        interview: stats.interview || 0,
+        declined: stats.declined || 0,
+    };
+    let monthlyApplications = [];
+    res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 export { createJob, updateJob, deleteJob, getAllJobs, showStats };
